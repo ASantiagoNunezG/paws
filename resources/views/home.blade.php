685564e8eb9b96f681a68b8000a7011163007057
Edilson <edilsonhuaman44@gmail.com>
2023-12-03 22:39:36 -0500

    Commit of Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,47 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <!-- SECCIÓN DE CARRUSEL -->
+    <div class="container-fluid p-0">
+        <div id="header-carousel" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="w-100" src="import/img/principal.jpg" alt="Image" preload="auto" loading="lazy">
+                    <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                        <div class="p-3" style="max-width: 900px;">
+                            <h1 class="display-3 text-white mb-3">En Perú hay más de 6 MILLONES de mascotas sin hogar</h1>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
+                <!-- Puedes agregar más elementos al carrusel según sea necesario -->
             </div>
         </div>
     </div>
-</div>
+    <!-- SECCIÓN DE CARRUSEL -->
+
+    <!-- SECCIÓN DE OBJETIVOS -->
+    <div class="container-fluid bg-light pt-5">
+        <div class="container py-5">
+            <div class="d-flex flex-column text-center mb-5">
+                <h1 class="display-4 m-0"><span class="text-primary">Nuestro</span> Objetivo</h1>
+            </div>
+            <div class="row pb-3">
+                <!-- Contenido de la sección de objetivos -->
+            </div>
+        </div>
+    </div>
+    <!-- SECCIÓN DE OBJETIVOS -->
+
+    <!-- SECCIÓN DE TESTIMONIOS -->
+    <div class="container-fluid bg-light my-5 p-0 py-5">
+        <div class="container p-0 py-5">
+            <div class="d-flex flex-column text-center mb-5">
+                <h1 class="display-4 m-0">Empresas<span class="text-primary"> Aliadas</span></h1>
+            </div>
+            <div class="d-flex flex-row">
+                <!-- Contenido de la sección de testimonios -->
+            </div>
+        </div>
+    </div>
+    <!-- SECCIÓN DE TESTIMONIOS -->
 @endsection
